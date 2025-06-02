7ed1a3b7b989d1e061e4e2dc562d05f5949470d2
Sander J. Bouwman <sjbouwman@users.noreply.github.com>
2025-06-02 09:44:43 +0200

    refactor: clean up unused code.

--- sqlaudit/decorators.py
+++ sqlaudit/decorators.py
@@ -3,12 +3,8 @@ from typing import TypeVar
 
 from sqlalchemy.orm import DeclarativeBase
 from sqlaudit.registry import SQLAuditOptions, audit_model_registry
-# from sqlaudit.config import get_audit_config
-# from sqlaudit.options import SQLAuditOptionsBase
-# from sqlaudit.pre_flight import run_preflight
 
 T = TypeVar("T", bound=DeclarativeBase)
-
 
 def track_table(
     *,
@@ -27,23 +23,6 @@ def track_table(
     def decorator(cls: type[T]) -> type[T]:
         if cls not in audit_model_registry:
             audit_model_registry.register(table_model=cls, options=options)
-
-
-        # config = get_audit_config()
-        # run_preflight(
-        #     cls,
-        #     SQLAuditOptionsBase(
-        #         tracked_fields=tracked_fields,
-        #         record_id_field=record_id_field,
-        #         user_id_field=user_id_field or config.default_user_id_column,
-        #         table_label=table_label,
-        #     ),
-        # )
-
-        # if not issubclass(cls, DeclarativeBase):
-        #     raise TypeError(
-        #         f"@track_table can only be applied to classes that are subclasses of DeclarativeBase. Got {type(cls)}."
-        #     )
 
         return cls
 
